eb37ec8eed13bc9aaf875af7193eda9dd2536129
Daniele <me@daniele.sh>
2026-03-22 15:28:19 +0100

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "an mtproto client written in zig";
+
+  inputs = {
+    nixpkgs.url = "https://channels.nixos.org/nixos-25.05/nixexprs.tar.xz";
+    flake-utils.url = "github:numtide/flake-utils";
+    zig.url = "github:mitchellh/zig-overlay";
+    zls.url = "github:zigtools/zls";
+
+    # Used for shell.nix
+    flake-compat = {
+      url = github:edolstra/flake-compat;
+      flake = false;
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  } @ inputs: let
+    overlays = [
+      # Other overlays
+      (final: prev: rec {
+        zigpkgs = inputs.zig.packages.${prev.system};
+        zig = inputs.zig.packages.${prev.system}."master-2026-03-20";
+        zls = inputs.zls.packages.${prev.system}.default;
+      })
+    ];
+
+    # Our supported systems are the same supported systems as the Zig binaries
+    systems = builtins.attrNames inputs.zig.packages;
+  in
+    flake-utils.lib.eachSystem systems (
+      system: let
+        pkgs = import nixpkgs {inherit overlays system;};
+      in rec {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            zig
+            zls
+          ];
+        };
+
+        # For compatibility with older versions of the `nix` binary
+        devShell = self.devShells.${system}.default;
+
+      }
+    );
+}
